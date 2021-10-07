451acb72c5771a2bbaccc60ef5f606606a89d8bd
Julian <home@Julians-MacBook-Pro.local>
2021-10-07 14:54:44 -0500

    '2021-Oct-07'

--- app/screens/main/detail.tsx
+++ app/screens/main/detail.tsx
@@ -19,15 +19,22 @@ import {
 } from 'native-base';
 
 
+
 export const DetailScreen: FC<StackScreenProps<NavigatorParamList, "detail">> = observer(
   ({ route, navigation }) => {
+
+    const _ref = useRef();
+
+    const scrollToTop = () => {
+      window.scrollTo(0, 0);
+    };
 
     const goBack = () => navigation.goBack()
     const profileScreen = () => navigation.navigate("profile")
     const listScreen = () => navigation.navigate("list")
 
     const passedParams: any = route.params || new Map();
-    var passedName: string = passedParams.name || 'Undefined name';
+    const passedName: string = passedParams.name || 'Undefined name';
 
     const { characterStore } = useStores()
     const { characters } = characterStore
@@ -41,6 +48,7 @@ export const DetailScreen: FC<StackScreenProps<NavigatorParamList, "detail">> = 
         }
       }
       // this is the fall back incase of error
+      // really ... just to shut up the linter haha
       const item: any = characters[0] || new Map();
       return [item, 0];
     }
@@ -50,6 +58,7 @@ export const DetailScreen: FC<StackScreenProps<NavigatorParamList, "detail">> = 
 
 
     function getRelatedItems(characters: any, itemCount: number, respQuant: number) {
+
 
       let relatedItems = [];
       let requests: any[]
@@ -78,7 +87,7 @@ export const DetailScreen: FC<StackScreenProps<NavigatorParamList, "detail">> = 
             flex={1}
             onPress={
               () => {
-                navigation.navigate('detail', { name: String(character.name) });
+                navigation.push('detail', { name: String(character.name) });
               }
             }
           >
@@ -142,6 +151,7 @@ export const DetailScreen: FC<StackScreenProps<NavigatorParamList, "detail">> = 
     }, [])
 
     return (
+
       <LayoutMain
         headerTx="common.companyNameCaps"
         onCenterPress={listScreen}
@@ -152,103 +162,103 @@ export const DetailScreen: FC<StackScreenProps<NavigatorParamList, "detail">> = 
         preset="scroll"
       >
 
-        <Box alignSelf={{ base: 'center', md: 'flex-start' }} >
+        <ScrollView ref={_ref} scrollToOverflowEnabled={true} >
+          <Box alignSelf={{ base: 'center', md: 'flex-start' }} >
 
-          <Heading mb={4} mt={10}>
-            {item.name}
-          </Heading>
+            <Heading mb={4} mt={10} key="HeadingMain">
+              {item.name}
+            </Heading>
 
-          <NativeText fontSize="xl" mb={4} >
-            in principio creavit Deus caelum et terram
-            terra autem erat inanis et vacua et tenebrae super faciem abyssi et spiritus Dei ferebatur super aquas
-            dixitque Deus fiat lux et facta est lux
-            et vidit Deus lucem quod esset bona et divisit lucem ac tenebras
-            appellavitque lucem diem et tenebras noctem factumque est vespere et mane dies unus
-          </NativeText>
+            <NativeText fontSize="xl" mb={4} >
+              in principio creavit Deus caelum et terram
+              terra autem erat inanis et vacua et tenebrae super faciem abyssi et spiritus Dei ferebatur super aquas
+              dixitque Deus fiat lux et facta est lux
+              et vidit Deus lucem quod esset bona et divisit lucem ac tenebras
+              appellavitque lucem diem et tenebras noctem factumque est vespere et mane dies unus
+            </NativeText>
 
-          <NativeText fontSize="xl" mb={4} >
-            dixit quoque Deus fiat firmamentum in medio aquarum et dividat aquas ab aquis
-            et fecit Deus firmamentum divisitque aquas quae erant sub firmamento ab his quae erant super firmamentum et factum est ita
-            vocavitque Deus firmamentum caelum et factum est vespere et mane dies secundus
-            dixit vero Deus congregentur aquae quae sub caelo sunt in locum unum et appareat arida factumque est ita
-          </NativeText>
-
-
-
-          <NativeImage
-            key={item.image}
-            h={64}
-            rounded="xl"
-            source={{
-              uri: item.image,
-            }}
-            alt={item.name}
-          />
+            <NativeText fontSize="xl" mb={4} >
+              dixit quoque Deus fiat firmamentum in medio aquarum et dividat aquas ab aquis
+              et fecit Deus firmamentum divisitque aquas quae erant sub firmamento ab his quae erant super firmamentum et factum est ita
+              vocavitque Deus firmamentum caelum et factum est vespere et mane dies secundus
+              dixit vero Deus congregentur aquae quae sub caelo sunt in locum unum et appareat arida factumque est ita
+            </NativeText>
 
 
-          <Heading mb={4} mt={10}>
-            Heading Two
-          </Heading>
+            <NativeImage
+              key={item.image}
+              h={64}
+              rounded="xl"
+              source={{
+                uri: item.image,
+              }}
+              alt={item.name}
+            />
 
-          <NativeText fontSize="xl" mb={4} >
-            et vocavit Deus aridam terram congregationesque aquarum appellavit maria et vidit Deus quod esset bonum
-            et ait germinet terra herbam virentem et facientem semen et lignum pomiferum faciens fructum iuxta genus suum cuius semen in semet ipso sit super terram et factum est ita
-            et protulit terra herbam virentem et adferentem semen iuxta genus suum lignumque faciens fructum et habens unumquodque sementem secundum speciem suam et vidit Deus quod esset bonum
-            factumque est vespere et mane dies tertius
-            dixit autem Deus fiant luminaria in firmamento caeli ut dividant diem ac noctem et sint in signa et tempora et dies et annos
-          </NativeText>
+            <Heading mb={4} mt={10}>
+              Heading Two
+            </Heading>
 
-          <NativeText fontSize="xl" mb={4} >
-            ut luceant in firmamento caeli et inluminent terram et factum est ita
-            fecitque Deus duo magna luminaria luminare maius ut praeesset diei et luminare minus ut praeesset nocti et stellas
-            et posuit eas in firmamento caeli ut lucerent super terram
-            et praeessent diei ac nocti et dividerent lucem ac tenebras et vidit Deus quod esset bonum
-            et factum est vespere et mane dies quartus
-          </NativeText>
+            <NativeText fontSize="xl" mb={4} >
+              et vocavit Deus aridam terram congregationesque aquarum appellavit maria et vidit Deus quod esset bonum
+              et ait germinet terra herbam virentem et facientem semen et lignum pomiferum faciens fructum iuxta genus suum cuius semen in semet ipso sit super terram et factum est ita
+              et protulit terra herbam virentem et adferentem semen iuxta genus suum lignumque faciens fructum et habens unumquodque sementem secundum speciem suam et vidit Deus quod esset bonum
+              factumque est vespere et mane dies tertius
+              dixit autem Deus fiant luminaria in firmamento caeli ut dividant diem ac noctem et sint in signa et tempora et dies et annos
+            </NativeText>
 
-          <NativeImage
-            key={item.name}
-            h={64}
-            rounded="xl"
-            source={{
-              uri: item.image,
-            }}
-            alt={item.name}
-          />
+            <NativeText fontSize="xl" mb={4} >
+              ut luceant in firmamento caeli et inluminent terram et factum est ita
+              fecitque Deus duo magna luminaria luminare maius ut praeesset diei et luminare minus ut praeesset nocti et stellas
+              et posuit eas in firmamento caeli ut lucerent super terram
+              et praeessent diei ac nocti et dividerent lucem ac tenebras et vidit Deus quod esset bonum
+              et factum est vespere et mane dies quartus
+            </NativeText>
 
-          <Heading mb={4} mt={10}>
-            Heading Three
-          </Heading>
+            <NativeImage
+              key={item.name}
+              h={64}
+              rounded="xl"
+              source={{
+                uri: item.image,
+              }}
+              alt={item.name}
+            />
 
-          <NativeText fontSize="xl" mb={4} >
-            dixit etiam Deus producant aquae reptile animae viventis et volatile super terram sub firmamento caeli
-            creavitque Deus cete grandia et omnem animam viventem atque motabilem quam produxerant aquae in species suas et omne volatile secundum genus suum et vidit Deus quod esset bonum
-            benedixitque eis dicens crescite et multiplicamini et replete aquas maris avesque multiplicentur super terram
-            et factum est vespere et mane dies quintus
-            dixit quoque Deus producat terra animam viventem in genere suo iumenta et reptilia et bestias terrae secundum species suas factumque est ita
-            et fecit Deus bestias terrae iuxta species suas et iumenta et omne reptile terrae in genere suo et vidit Deus quod esset bonum
-          </NativeText>
+            <Heading mb={4} mt={10}>
+              Heading Three
+            </Heading>
 
-          <NativeText fontSize="xl" mb={4} >
-            et ait faciamus hominem ad imaginem et similitudinem nostram et praesit piscibus maris et volatilibus caeli et bestiis universaeque terrae omnique reptili quod movetur in terra
-            et creavit Deus hominem ad imaginem suam ad imaginem Dei creavit illum masculum et feminam creavit eos
-            benedixitque illis Deus et ait crescite et multiplicamini et replete terram et subicite eam et dominamini piscibus maris et volatilibus caeli et universis animantibus quae moventur super terram
-            dixitque Deus ecce dedi vobis omnem herbam adferentem semen super terram et universa ligna quae habent in semet ipsis sementem generis sui ut sint vobis in escam
-            et cunctis animantibus terrae omnique volucri caeli et universis quae moventur in terra et in quibus est anima vivens ut habeant ad vescendum et factum est ita
-            viditque Deus cuncta quae fecit et erant valde bona et factum est vespere et mane dies sextus
-          </NativeText>
+            <NativeText fontSize="xl" mb={4} >
+              dixit etiam Deus producant aquae reptile animae viventis et volatile super terram sub firmamento caeli
+              creavitque Deus cete grandia et omnem animam viventem atque motabilem quam produxerant aquae in species suas et omne volatile secundum genus suum et vidit Deus quod esset bonum
+              benedixitque eis dicens crescite et multiplicamini et replete aquas maris avesque multiplicentur super terram
+              et factum est vespere et mane dies quintus
+              dixit quoque Deus producat terra animam viventem in genere suo iumenta et reptilia et bestias terrae secundum species suas factumque est ita
+              et fecit Deus bestias terrae iuxta species suas et iumenta et omne reptile terrae in genere suo et vidit Deus quod esset bonum
+            </NativeText>
 
-
-          <Heading mb={4} mt={10}>
-            More Content
-          </Heading>
-          <HStack mx={{ base: 'auto', md: 0 }} space={2} mb={8} >
-            {relatedItems}
-          </HStack>
+            <NativeText fontSize="xl" mb={4} >
+              et ait faciamus hominem ad imaginem et similitudinem nostram et praesit piscibus maris et volatilibus caeli et bestiis universaeque terrae omnique reptili quod movetur in terra
+              et creavit Deus hominem ad imaginem suam ad imaginem Dei creavit illum masculum et feminam creavit eos
+              benedixitque illis Deus et ait crescite et multiplicamini et replete terram et subicite eam et dominamini piscibus maris et volatilibus caeli et universis animantibus quae moventur super terram
+              dixitque Deus ecce dedi vobis omnem herbam adferentem semen super terram et universa ligna quae habent in semet ipsis sementem generis sui ut sint vobis in escam
+              et cunctis animantibus terrae omnique volucri caeli et universis quae moventur in terra et in quibus est anima vivens ut habeant ad vescendum et factum est ita
+              viditque Deus cuncta quae fecit et erant valde bona et factum est vespere et mane dies sextus
+            </NativeText>
 
 
-        </Box>
+            <Heading mb={4} mt={10}>
+              More Content
+            </Heading>
+            <HStack mx={{ base: 'auto', md: 0 }} space={2} mb={8} >
+              {relatedItems}
+            </HStack>
 
+
+          </Box>
+
+        </ScrollView>
 
       </LayoutMain>
 
